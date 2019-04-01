0c09bc622ddad1cddd0337b5ff964d9f4fff0183
Ryller Oliveira <rylleroliveira@gmail.com>
2019-04-01 15:04:49 -0300

    rh-app - implementação de campos e inicio de datatable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  dataSource: ['Apresentação e postura' , 'Comunicação oral', 'Comunicação gestual', 'Comunicação vosial',
+    'Organização e estrutura de aula', 'Clima da aula', 'Clareza nas explicações', 'Controle do tempo',
+    'Seguraça', 'Conhecimento do conteúdo'];
+
   constructor() { }
 
   ngOnInit() {
